aa44860c7a37da163519e315928d9754387a9518
Sanjay <sanjay.narayana@hotmail.com>
2015-12-06 03:15:03 -0800

    Reading from stdin now, completed all back-end work

--- train_game.py
+++ train_game.py
@@ -6,27 +6,32 @@ numbers_to_permute = []
 
 operations = ['+','-','*','/']
 
-for arg in sys.argv[1:]:
-    numbers_to_permute.append(arg)
+print("Please enter 4 numbers: ")
+numbers_to_permute.append(int(input("number 1: ")))
+numbers_to_permute.append(int(input("number 2: ")))
+numbers_to_permute.append(int(input("number 3: ")))
+numbers_to_permute.append(int(input("number 4: ")))
 
+print("Enter a goal number: ")
+goal = int(input("goal: "))
 goal = 10
 
 num = list(itertools.permutations(numbers_to_permute))
-oper = list(itertools.combinations_with_replacement(operations,3))
+oper = list(itertools.combinations_with_replacement(operations,len(operations)-1))
+
 
 def myZip(num, oper):
     expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
     return round(eval(expr))
       
+
 def solved(num, oper):
     expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
     return expr
-          
+
+
 for o in oper:
     for n in num:
         result = myZip(n,o)
         if result == goal:
             print(solved(n,o), "=", goal)
-        
-    
-

6581be7246fa3320a6558f4fdd0a1e56713baf80
Sanjay <sanjay.narayana@hotmail.com>
2015-12-05 19:31:33 -0800

    can generate combinations of operations

--- train_game.py
+++ train_game.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
+import itertools
+
 myList = [1,3,5,6]
+
+# Explanation of yield keyword:
+# an iterator is used to describe an object that has a next() method.
+# a yield allows a function to be iterated.
 
 def all_perms(elements):
     if len(elements) <=1:
@@ -11,8 +17,12 @@ def all_perms(elements):
                 # nb elements[0:1] works in both string and list contexts
                 yield perm[:i] + elements[0:1] + perm[i:]
             
-y = list(all_perms(myList))
+num = list(all_perms(myList))
 
+oper = list(itertools.combinations_with_replacement(['+','-','*','/'],3))
 
-for i in y:
+for i in oper:
+    print(i)
+    
+for i in num:
     print(i)

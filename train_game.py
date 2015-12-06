391adc6098e30b6152cc633d82c6376df7b1971d
Sanjay <sanjay.narayana@hotmail.com>
2015-12-06 02:35:34 -0800

    completed calculations

--- train_game.py
+++ train_game.py
@@ -14,3 +14,19 @@ goal = 10
 num = list(itertools.permutations(numbers_to_permute))
 oper = list(itertools.combinations_with_replacement(operations,3))
 
+def myZip(num, oper):
+    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
+    return round(eval(expr))
+      
+def solved(num, oper):
+    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
+    return expr
+          
+for o in oper:
+    for n in num:
+        result = myZip(n,o)
+        if result == goal:
+            print(solved(n,o), "=", goal)
+        
+    
+

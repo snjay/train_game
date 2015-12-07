0dd31812bcbc5f86a9ee4fc4199d69211bc71b60
Sanjay <sanjay.narayana@hotmail.com>
2015-12-06 17:57:46 -0800

    re-adding some features

--- train_game.py
+++ train_game.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
-import sys, re, itertools
+import itertools
 
 numbers_to_permute = []
-
 operations = ['+','-','*','/']
+
+def myZip(num, oper):
+    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
+    return round(eval(expr))
+      
+def solved(num, oper):
+    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
+    return expr
 
 print("Please enter 4 numbers: ")
 numbers_to_permute.append(int(input("number 1: ")))
@@ -14,22 +21,13 @@ numbers_to_permute.append(int(input("number 4: ")))
 
 print("Enter a goal number: ")
 goal = int(input("goal: "))
-goal = 10
 
+# return permutation of every number
 num = list(itertools.permutations(numbers_to_permute))
+# return a combination (with replacement) of every operator
 oper = list(itertools.combinations_with_replacement(operations,len(operations)-1))
 
-
-def myZip(num, oper):
-    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
-    return round(eval(expr))
-      
-
-def solved(num, oper):
-    expr = str(num[0]) + oper[0] + str(num[1]) + oper[1] + str(num[2]) + oper[2] + str(num[3])
-    return expr
-
-
+# main calculations
 for o in oper:
     for n in num:
         result = myZip(n,o)
